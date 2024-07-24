77ad96cbb90d92e3be00dc755e3f0523b68f28b0
Petro Malamule <133478698+Shaunpetro@users.noreply.github.com>
2024-07-24 17:53:24 +0200

    code fix

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,8 +1,8 @@
-functionsignUpUser(FirstName, lastName) {
-    return Promise.resolve({
-        firstName,
-        lastName,
-    });
+function signUpUser(firstName, lastName) {
+  return Promise.resolve({
+    firstName,
+    lastName,
+  });
 }
 
 export default signUpUser;
