8ae4badbe1d27b2e0c015f64ffc41db54266c7d7
Petro Malamule <133478698+Shaunpetro@users.noreply.github.com>
2024-07-25 17:17:19 +0200

    code fixes

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,48 +1,52 @@
 export default class HolbertonCourse {
-    constructor(name, length, students) {
-        if (typeof name !== 'string') {
-            throw new TypeError('Name must be a string');
-        } else {
-            this._name = name;
-        }
-        if (typeof length !== 'number') {
-            throw new TypeError('Length must be a number');
-        } else {
-            this._length = length;
-        }
-        if (!Array.isArray(students)) {
-            throw new TypeError('must be a array');
-        } else {
-            this._students = students;
-        }
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    } else {
+      this._name = name;
     }
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    } else {
+      this._length = length;
+    }
+    if (!Array.isArray(students)) {
+      throw new TypeError('must be a array');
+    } else {
+      this._students = students;
+    }
+  }
 
-    get name() {
-        return this._name;
-    }
+  get name() {
+    return this._name;
+  }
 
-    set name(na) {
-        if (typeof na !== 'string') {
-            throw new TypeError('Name must be a string');
-        }
-        this._name = na;
+  set name(na) {
+    if (typeof na !== 'string') {
+      throw new TypeError('Name must be a string');
     }
+    this._name = na;
+  }
 
-    set length(zise) {
-        if (typeof zise !== 'number') {
-            throw new TypeError('Length must be a number');
-        }
-        this._length = zise;
-    }
+  get length() {
+    return this._length;
+  }
 
-    get students() {
-        return this._students;
+  set length(zise) {
+    if (typeof zise !== 'number') {
+      throw new TypeError('Length must be a number');
     }
+    this._length = zise;
+  }
 
-    set students(stu) {
-        if (!Array.isArray(stu)) {
-            throw new TypeError('must be a array');
-        }
-        this._students = stu;
+  get students() {
+    return this._students;
+  }
+
+  set students(stu) {
+    if (!Array.isArray(stu)) {
+      throw new TypeError('must be a array');
     }
+    this._students = stu;
+  }
 }
