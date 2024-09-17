56e153b41c1ff8b83ce6612c23dedf0e7f1f7f64
Petro Malamule <133478698+Shaunpetro@users.noreply.github.com>
2024-09-17 19:03:33 +0200

    format update

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,13 +1,12 @@
-const { response } = require('express');
 const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = 1245;
 
 const app = http.createServer((request, response) => {
-    response.statusCode = 200;
-    response.setHeader('Content-Type', 'text/plain');
-    response.end('Hello Holberton School!')
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain');
+  response.end('Hello Holberton School!');
 });
 
 app.listen(port, hostname, () => {
